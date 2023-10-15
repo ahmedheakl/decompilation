0b46597234eab2c3ca65a0aab1940ef94ac2ffec
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-10-15 19:46:59 +0300

    Edited after Review
    Fixed docstring
    
    Added comments to explain function for further documentation
    
    Improved removing white space using re
    
    Added one TODO for future

--- decompile/preprossing/standardize.py
+++ decompile/preprossing/standardize.py
@@ -9,25 +9,33 @@ def standardize_asm_file(asm_file_path: str) -> str:
         asm_file_path(str): path to asm file.
 
     Returns:
-        str: standardized asm file as text."""
+        str: standardized asm file as text.
+    """
     section_start_text = "Disassembly of section"
-    memory_address_format_example = "48 c7 45 f8 00 00 00    "
-    instruction_split_regex = " "
+    length_memory_address_format_example = len(
+        "48 c7 45 f8 00 00 00    "
+    )  # from objdump output, need to know length to remove it from start of instruction.
     standardized_asm_buffer = ""
-    with open(asm_file_path, "r") as f:
-        for line in f.readlines()[4:]:
+
+    with open(asm_file_path, "r") as file:
+        for line in file.readlines()[4:]:
             line = line.strip()
+
             if not line or line.startswith(section_start_text):
                 continue
+
+            # TODO: Use a regex to find "<section_name>:" or just detect them after section_start_text
             if line.startswith("<"):
                 standardized_asm_buffer += line + "\n"
+
             else:
-                line = line[len(memory_address_format_example) - 2 :]
+                line = line[
+                    length_memory_address_format_example - 2 :
+                ]  # remove memory address from start of instruction.
                 if not line:
                     continue
-                components = re.split(instruction_split_regex, line)
-                components = [c for c in components if c]
-                line = " ".join(components)
-                line = " , ".join(line.split(","))
+                line = re.sub(r"\s+", " ", line).strip()  # remove extra spaces
+                line = " , ".join(line.split(","))  # add space before and after comma
                 standardized_asm_buffer += line + " ;" + "\n"
+
     return standardized_asm_buffer
