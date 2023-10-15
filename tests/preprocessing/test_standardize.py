cef8dffe8823a30279dbc60ccaa2b8ed5066e489
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-10-15 19:28:24 +0300

    Changed path from str to Path object
    Also added encoding for test function.

--- tests/preprocessing/test_standardize.py
+++ tests/preprocessing/test_standardize.py
@@ -1,9 +1,16 @@
 from decompile.preprossing.standardize import standardize_asm_file
+from pathlib import Path
+
+TESTIING_DATA_FOLDER = Path("tests/tests_data/preprocessing")
 
 
 def test_standardize():
-    with open("tests/tests_data/preprocessing/standardize_test_output_1.s", "r") as f:
-        output_text = f.read()
+    with open(
+        TESTIING_DATA_FOLDER / "standardize_test_output_1.s",
+        "r",
+        encoding="utf-8",
+    ) as file:
+        output_text = file.read()
     assert (
         standardize_asm_file(
             "tests/tests_data/preprocessing/standardize_test_input_1.s"
