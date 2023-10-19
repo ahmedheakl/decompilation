332fe930337997d35de0f4ea193182cecc6ab495
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-10-20 00:06:21 +0300

    Edited/generalized paths in preprocessing script and Added datasets/ to .gitignore
    Fixed datasets folder structure to be relative instead of absolute
    
    Provided safe exception raising if dataset doesn't exist
    
    Changed all Path.as_posix() methods to the more general str(Path)

--- preprocess_angha_dataset.py
+++ preprocess_angha_dataset.py
@@ -3,28 +3,33 @@ from pathlib import Path
 
 from decompile.preprocessing import preprocess
 
-anghaBench_dataset_folder = Path("/home/mohanned/Work/AnghaBench")
-input_folder = Path("/home/mohanned/Work/DataC")
-output_folder = Path("/home/mohanned/Work/Binary")
+DATASET_NAME = "AnghaBench"
+anghaBench_dataset_folder = Path(f"./datasets/raw/{DATASET_NAME}")
+input_folder = Path(f"./datasets/formatted/{DATASET_NAME}/input")
+output_folder = Path(f"./datasets/formatted/{DATASET_NAME}/output")
 ARCHITECTURE = "x86-64"
 SYNTAX_TYPE = "att"
 
 
 def main() -> None:
     """Main entry point for preprocessing the anghadataset"""
+    if not anghaBench_dataset_folder.exists():
+        raise FileNotFoundError(
+            f"AngaBench dataset folder not found at {anghaBench_dataset_folder}"
+        )
     if not input_folder.exists():
-        input_folder.mkdir(parents=True, exist_ok=True)
+        input_folder.mkdir(parents=True)
 
     if not output_folder.exists():
-        output_folder.mkdir(parents=True, exist_ok=True)
+        output_folder.mkdir(parents=True)
 
     preprocess.collect_c_files(
-        anghaBench_dataset_folder.as_posix(),
-        input_folder.as_posix(),
+        str(anghaBench_dataset_folder),
+        str(input_folder),
     )
     preprocess.preprocess(
-        input_folder.as_posix(),
-        output_folder.as_posix(),
+        str(input_folder),
+        str(output_folder),
         number_of_samples=1000,
         number_of_processor_cores=4,
         syntax_for_assembly_language=SYNTAX_TYPE,
